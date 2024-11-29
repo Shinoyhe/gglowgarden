13056ea8d2e0301fc6d25349922e9d35aefa1da3
Jonah Ryan <jonahrobot@gmail.com>
2024-11-28 20:57:51 -0800

    Add funny talk sounds

--- Assets/Scripts/Dialouge/DisplayText.cs
+++ Assets/Scripts/Dialouge/DisplayText.cs
@@ -5,12 +5,29 @@ using TMPro;
 
 public class DisplayText : MonoBehaviour
 {
+    [Header("Dependencies")]
     public TMP_Text nameText;
     public TMP_Text dialogueText;
 
+    [Header("Audio Clips")]
+    public AudioClip[] talkingClips;
+    public AudioClip punctuation;
+
+    [Header("Speaking Settings")]
+    public float minPitch = 0.8f;
+    public float maxPitch = 0.9f;
+    public float textSpeed = 0.05f;
+
+
     private int charactersForThisLine = 0;
+    private AudioSource source;
     [HideInInspector]
     public bool finishedTypingText = false;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
 
     public void setupDialogueText(string text)
     {
@@ -38,7 +55,26 @@ public class DisplayText : MonoBehaviour
 
     IEnumerator nextCharacter()
     {
-        yield return new WaitForSeconds(.05f);
+        int index = Mathf.Clamp(dialogueText.maxVisibleCharacters, 0, dialogueText.text.Length - 1);
+
+        char currentCharacter = dialogueText.text[index];
+
+        float actualTextSpeed = textSpeed;
+
+        if (currentCharacter == '.')
+        {
+            actualTextSpeed *= 5;
+        }
+
+        yield return new WaitForSeconds(actualTextSpeed);
+
+        // Play sound every other character or if a punctuation
+        if (currentCharacter == '.' || dialogueText.maxVisibleCharacters % 2 == 0)
+        { 
+            playTalkSound(currentCharacter);
+        }
+
+        // Get next character
         dialogueText.maxVisibleCharacters += 1;
 
         if(dialogueText.maxVisibleCharacters >= charactersForThisLine)
@@ -48,6 +84,26 @@ public class DisplayText : MonoBehaviour
         else
         {
             StartCoroutine("nextCharacter");
+        }
+    }
+
+    void playTalkSound(char currentCharacter)
+    {
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+
+        source.pitch = Random.Range(minPitch, maxPitch);
+
+        if (char.IsLetter(currentCharacter))
+        {
+            int audioIndex = currentCharacter % talkingClips.Length;
+            source.PlayOneShot(talkingClips[audioIndex]);
+        }
+        else
+        {
+            source.PlayOneShot(punctuation);
         }
     }
 }
