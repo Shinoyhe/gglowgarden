f5881358112c7b78e2ba6b64f26da0be534f1e78
Jonah Ryan <jonahrobot@gmail.com>
2024-11-28 22:07:38 -0800

    Add tag parsing in ChatNode strings

--- Assets/Scripts/Dialouge/ChatNode.cs
+++ Assets/Scripts/Dialouge/ChatNode.cs
@@ -1,8 +1,5 @@
-using OpenCover.Framework.Model;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -119,11 +116,83 @@ public class ChatNode : MonoBehaviour, IInteractable
     {
         string currentTextLine = currentChat.lines[currentChat.currentLine];
 
+        Tags currentTags = getTags(currentTextLine);
+
         currentChat.currentLine += 1;
 
+        string currentText = removeTags(currentTextLine);
+
         // Setup our dialouge
-        dialougesDisplayText.setupNameText(currentChat.name);
-        dialougesDisplayText.setupDialogueText(currentTextLine);
+        dialougesDisplayText.setupNameText(currentTags.name);
+        dialougesDisplayText.setupDialogueText(currentText);
+    }
+
+    public struct Tags
+    {
+        public string name;
+
+        public Tags(string Name = "")
+        {
+            name = Name;
+        }
+    }
+
+    public Tags getTags(string currentLine)
+    {
+        Tags newTags = new Tags();
+
+        // Get tags with structure [KEY,var,var] [tags,var,var]
+        Regex regex = new Regex(@"\[(.*?)\]");
+        MatchCollection matches = regex.Matches(currentLine);
+
+        // For each [KEY,var] match
+        for (int i = 0; i < matches.Count; i++)
+        {
+            // Get each part of match
+            string[] tagParts = matches[i].Groups[1].Value.Split(',');
+            string tagKey = tagParts[0].ToLower();
+
+            switch (tagKey)
+            {
+                case "name":
+                    if (tagParts.Length != 2) break;
+                    newTags.name = removeStartingSpace(tagParts[1]);
+                    break;
+            }
+
+        }
+
+        return newTags;
+    }
+
+    public string removeStartingSpace(string input)
+    {
+        if (input[0] == ' ')
+        {
+            input = input.Substring(1);
+        }
+
+        return input;
+    }
+
+    public string removeTags(string currentLine)
+    {
+        // Get tags with structure [KEY,var,var] [tags,var,var]
+        Regex regex = new Regex(@"\[(.*?)\]");
+        MatchCollection matches = regex.Matches(currentLine);
+
+        // Get the last match
+        Match lastMatch = matches[matches.Count - 1];
+
+        // Get the index of the character after the last match
+        int startIndex = lastMatch.Index + lastMatch.Length;
+
+        // Extract the remaining substring
+        string remainingString = currentLine.Substring(startIndex);
+
+        remainingString = removeStartingSpace(remainingString);
+
+        return remainingString;
     }
 
     public void endChat()
